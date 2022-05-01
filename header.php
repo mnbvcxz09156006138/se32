d0ca4aa8a32de44f802c678cf8c3bf0a463d7a67
mohamad.nematizadeh <89206040+mnbvcxz09156006138@users.noreply.github.com>
2022-05-01 18:27:29 +0430

    Add files via upload

---
+++ header.php
@@ -0,0 +1,35 @@
+
+<html lang="fa" dir="rtl" style="height: 100%;">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Keep</title>
+    <link rel="icon" type="image/png" href="2.png">
+    <link rel="stylesheet" href="bootstrap.rtl.css">
+    <link rel ="stylesheet" type="text/css" href=" css/all.css"> 
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+  <nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="keep_2020q4_48dp.png" alt="" height="40" class="d-inline-block align-text-top">
+      Google Keep
+    </a>
+  </div>
+</nav>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"> خانه</a>
+        </li>
+ 
+    </div>
+  </div>
+</nav>
+<boy
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+include "header.php";
+include "database.php";
+
+$tasks = $db->query("SELECT * FROM taskdata")
+?>
+<
+<div class="card">
+
+  <div class="card-body">
+  <form method="post" action="add_task.php">         
+                    <label for="exampleInputPassword1" class="form-label"> عنوان خود را وارد کنید</label>
+                        <div class="form-floating">
+                        <input type="text" name="title" class="form-control " data-bs-toggle="collapse" data-bs-target="#collapseWidthExample">
+                        <label for="floatingTextarea">title</label>
+                        </div>
+
+                       <label for="exampleInputPassword1" class="form-label"> تسک خود را وارد کنید</label>
+                        <div class="form-floating">
+                        <input type="text" name="text" class="form-control " data-bs-toggle="collapse" data-bs-target="#collapseWidthExample">
+                        <label for="floatingTextarea">task</label>
+                        </div>
+               
+                        <label for="exampleInputPassword1" class="form-label"> رنگ </label>
+                        <div class="form-floating">
+                            <input type="color" name="color" value="#063d7c" >
+                        </div>
+                        <label for="exampleInputPassword1" class="form-label"> تاریخ   </label>
+                        <div class="col-3">
+                            <input type="date" value="" name="date" >
+                        </div>
+                        <label for="exampleInputPassword1" class="form-label"> زمان   </label>
+                        <div class="col-3">
+                        <input type="time" value="" name="time" >
+                        </div>
+                        <br>
+                
+                    </form>
+ </div>
+ <button type="submit" class="btn btn-primary">ذخیره</button>
+  </div>
+</div>               
+ <div class="row mt-5" >
+            <?php foreach ($tasks as $task): ?>
+                <div class=" col-lg-3 col-md-6 col-sm-12 col-12 position-relative">
+                    <div class="card text-white mb-3 text-start" style="background:<?php echo $task["color"] ?>; ">
+                        <div class="card-header stroke "> <h4 class="d-block"><?php echo $task["title"] ?> </h4>
+                        </div>
+                        <div class="card-body ">
+                            <h5 class="card-text mb-3 stroke " id="text_note"><?php echo $task["text"] ?></h5>
+                            <p class="card-text mb-2 stroke fs-6"> <i class="far fa-alarm-clock"></i> <?php echo $task["time"] ?> </p>
+                            <p class="card-text  stroke fs-6"><i class="far fa-calendar-alt"></i>  <?php echo $task["date"] ?> </p>
+                        </div>
+                       
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+        </div>
+    
+       
+<?php
+
+include "footer.php"
+
+?>
+
